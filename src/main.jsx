3669e1a64b8cba5061e6bf55d9c87d9c81b10610
rifat028 <istiakrifat001@gmail.com>
2025-11-13 03:19:28 +0600

    AuthProvider Initiated

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import router from "./Routes/PublicRoutes.jsx";
 import { RouterProvider } from "react-router/dom";
+import AuthProvider from "./Authentication/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <AuthProvider>
+      <RouterProvider router={router} />,
+    </AuthProvider>
   </StrictMode>
 );
