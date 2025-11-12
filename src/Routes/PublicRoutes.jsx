4bad933f6c934cfaba11716a1281a7fdf4a858e3
rifat028 <istiakrifat001@gmail.com>
2025-11-12 20:07:07 +0600

    Basic Public router

--- src/Routes/PublicRoutes.jsx
+++ src/Routes/PublicRoutes.jsx
@@ -1,8 +1,42 @@
 import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import MyBooks from "../Pages/MyBooks";
+import AddBooks from "../Pages/AddBooks";
+import AllBooks from "../Pages/AllBooks";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Home,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/Register",
+        Component: Register,
+      },
+      {
+        path: "/Register",
+        Component: Register,
+      },
+      {
+        path: "/all-books",
+        Component: AllBooks,
+      },
+      {
+        path: "/my-books",
+        Component: MyBooks,
+      },
+      {
+        path: "/add-books",
+        Component: AddBooks,
+      },
+    ],
   },
 ]);
+
+export default router;
